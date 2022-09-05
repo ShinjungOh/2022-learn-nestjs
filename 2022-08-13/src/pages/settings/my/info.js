d61205c87c1eedc3be9ef6b731e55b46a6566b2b
ShinjungOh <tlswjd9260@gmail.com>
2022-09-05 23:52:35 +0900

    [ADD] shallow routing

--- 2022-08-13/src/pages/settings/my/info.js
+++ 2022-08-13/src/pages/settings/my/info.js
@@ -1,11 +1,48 @@
 import styles from '../../../../styles/Home.module.css'
 import Layout from "components/Layout";
 import SubLayout from "components/SubLayout";
+import {useState} from "react";
+import {useRouter} from "next/router";
+
+export async function getServerSideProps() {
+    console.log('server')
+
+    return {
+        props: {},
+    }
+}
 
 export default function MyInfo() {
+    const router = useRouter();
+    const [clicked, setClicked] = useState(false);
+    const {status = 'initial'} = router.query;
+
     return (
         <>
             <h1 className={styles.title}>My Info</h1>
+            <h1 className={styles.title}>Clicked: {String(clicked)}</h1>
+            <h1 className={styles.title}>Status: {status}</h1>
+            <button onClick={() => {
+                alert('edit');
+                setClicked(true);
+                location.replace('/settings/my/info?status=editing');
+            }}>edit(replace)
+            </button>
+            <br/>
+            <button onClick={() => {
+                alert('edit');
+                setClicked(true);
+                router.push('/settings/my/info?status=editing');
+            }}>edit(push)
+            </button>
+            <br/>
+            <button onClick={() => {
+                alert('edit');
+                setClicked(true);
+                router.push('/settings/my/info?status=editing', undefined, {shallow: true});
+            }}>edit(shallow)
+            </button>
+
         </>
     )
 }
