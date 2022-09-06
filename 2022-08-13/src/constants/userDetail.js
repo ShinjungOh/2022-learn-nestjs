9f49c99c206c45fa1ece19feb4326ad22d15063b
ShinjungOh <tlswjd9260@gmail.com>
2022-09-06 22:07:01 +0900

    [ADD] API routes

---
+++ 2022-08-13/src/constants/userDetail.js
@@ -0,0 +1,6 @@
+export const userDetail = {
+    name: 'shinjung',
+    age: 20,
+    height: '100m',
+    weight: '500g',
+}
--- 2022-08-13/src/pages/[username]/[info].js
+++ 2022-08-13/src/pages/[username]/[info].js
@@ -1,15 +1,36 @@
 import styles from '../../../styles/Home.module.css'
 import Layout from "components/Layout";
 import SubLayout from "components/SubLayout";
-import { useRouter } from 'next/router';
+import {useRouter} from 'next/router';
+import {useEffect, useState} from "react";
 
 export default function UsernameInfo() {
     const router = useRouter();
-    const {username, info} = router.query;
+    const {username, info, uid} = router.query;
+    const [name, setName] = useState('?');
+
+    // useEffect(() => {
+    //     fetch('/api/user')
+    //         .then((res) => res.json())
+    //         .then((data) => {
+    //             setName(data.name)
+    //         })
+    // }, []);
+
+    useEffect(() => {
+        if (uid !== null) {
+            fetch(`/api/user-info/${uid}`)
+                .then((res) => res.json())
+                .then((data) => {
+                    setName(data.name)
+                })
+        }
+    }, [uid]);
 
     return (
         <>
             <h1 className={styles.title}>{username}'s {info}</h1>
+            <h1 className={styles.title}>name: {name}</h1>
         </>
     )
 }
